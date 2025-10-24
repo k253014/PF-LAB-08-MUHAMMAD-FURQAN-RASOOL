65a592a0b026219363f1b42fd04d4a94bd8f3d36
Muhammad Furqan Rasool <k253014@nu.edu.pk>
2025-10-23 21:21:29 -0700

    Add files via upload

---
+++ TASK1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main () {
+	int n;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	
+	 for(int i=1;i<=n;i++) {
+        printf("%d: ", i);
+        for(int j=1;j<=10;j++) {
+            int total = i * j;
+            printf("%d ", total);
+        }
+        printf("\n");
+    }
+}
