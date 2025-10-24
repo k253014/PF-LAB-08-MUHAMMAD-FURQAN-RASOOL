a922a95bbe9e2735903334458346cd1bfa0d3393
Muhammad Furqan Rasool <k253014@nu.edu.pk>
2025-10-23 21:23:46 -0700

    Update TASK1.c

--- TASK1.c
+++ TASK1.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 
 int main () {
-	int n;
+	int n,i,j;
 	printf("Enter a number: ");
 	scanf("%d", &n);
 	
-	 for(int i=1;i<=n;i++) {
+	 for(i=1;i<=n;i++) {
         printf("%d: ", i);
-        for(int j=1;j<=10;j++) {
+        for( j=1;j<=10;j++) {
             int total = i * j;
             printf("%4d ", total);
         }
         printf("\n");
     }
 }
-
